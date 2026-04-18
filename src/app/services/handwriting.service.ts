f7936aa3ed27fe43dbecd65e49d7a618723231da
tranthaibinh1998 <tranthaibinh1998@gmail.com>
2026-04-19 00:11:56 +0900

    build

--- src/app/services/handwriting.service.ts
+++ src/app/services/handwriting.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
+
 export interface HandwritingRecognizeRequest {
   imageBase64: string;
   language: string;
