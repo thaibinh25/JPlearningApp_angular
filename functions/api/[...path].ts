bc61e384f69051d4582953025d4d43da3fb76050
tranthaibinh1998 <tranthaibinh1998@gmail.com>
2026-04-19 00:19:36 +0900

    update

--- functions/api/[...path].ts
+++ functions/api/[...path].ts
@@ -1,18 +1,21 @@
 export async function onRequest(context: any) {
   const { request, params } = context;
 
-  const backendBase = "http://jplearning-backend-env.eba-zmn92mjm.ap-southeast-2.elasticbeanstalk.com";
-
+  const backendBase = 'http://jplearning-backend-env.eba-zmn92mjm.ap-southeast-2.elasticbeanstalk.com';
   const url = new URL(request.url);
-  const path = params.path ? params.path.join("/") : "";
+  const path = params.path ? params.path.join('/') : '';
 
   const targetUrl = `${backendBase}/api/${path}${url.search}`;
 
-  const newRequest = new Request(targetUrl, {
+  const init: RequestInit = {
     method: request.method,
     headers: request.headers,
-    body: request.method === "GET" ? null : request.body
-  });
+    redirect: 'follow'
+  };
 
-  return fetch(newRequest);
+  if (request.method !== 'GET' && request.method !== 'HEAD') {
+    init.body = request.body;
+  }
+
+  return fetch(targetUrl, init);
 }
