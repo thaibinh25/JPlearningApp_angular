00aebb75edf7ab75298cba664041fc9f1ccef49c
tranthaibinh1998 <tranthaibinh1998@gmail.com>
2026-04-19 02:11:30 +0900

    u

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://jplearning-backend-env.eba-zmn92mjm.ap-southeast-2.elasticbeanstalk.com/api'
+  apiBaseUrl: 'https://jplearning.duckdns.org/api'
 };
