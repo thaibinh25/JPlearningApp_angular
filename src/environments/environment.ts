a8760ec38dfae7fe1f2c8226a92fcae1c07bfecb
tranthaibinh1998 <tranthaibinh1998@gmail.com>
2026-04-19 00:56:12 +0900

    O

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://jplearning-backend-env.eba-zmn92mjm.ap-southeast-2.elasticbeanstalk.com/api'
+  apiBaseUrl: 'https://jplearning-backend-env.eba-zmn92mjm.ap-southeast-2.elasticbeanstalk.com/api'
 };
