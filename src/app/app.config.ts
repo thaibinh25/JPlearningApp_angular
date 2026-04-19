538b0dec91f0222bbeb55d1c2c704ffe01d39867
tranthaibinh1998 <tranthaibinh1998@gmail.com>
2026-04-19 15:03:01 +0900

    ipad,iphone

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled'
+      })),
     provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
